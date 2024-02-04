2027e75cc3fe34034b181ac4c8338c7ef1eef0cd
iamelDuderino <andrew.j.tomko@gmail.com>
2024-02-04 15:23:45 -0500

    Update main.go

--- main.go
+++ main.go
@@ -8,28 +8,5 @@ import (
 )
 
 func main() {
-
-	// download resume
-	// uri := "https://" + os.Getenv("GITHUB_TOKEN") + "@raw.githubusercontent.com/" + os.Getenv("PATH_TO_PRIVATE_REPO")
-	// req, err := http.NewRequest(http.MethodGet, uri, nil)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// out, err := os.Create("resume.json")
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// client := &http.Client{}
-	// resp, err := client.Do(req)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// _, err = io.Copy(out, resp.Body)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// resp.Body.Close()
-
 	log.Fatal(http.ListenAndServe(app.Run.Server.ListenAddress, app.Run.Server.Mux))
-
 }
